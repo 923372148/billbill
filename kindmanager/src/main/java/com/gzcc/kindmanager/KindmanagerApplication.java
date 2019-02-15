c70a263bae07fab54b794aaf5d7306a992b6e530
zhuoyujing <zhuoyj@hopnetwork.com>
2019-02-15 18:00:49 +0800

    [0.0.6]	:sparkles: 增加类别模块

---
+++ kindmanager/src/main/java/com/gzcc/kindmanager/KindmanagerApplication.java
@@ -0,0 +1,14 @@
+package com.gzcc.kindmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindmanagerApplication.class, args);
+    }
+
+}
+
